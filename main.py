a2f5fd697c4e15d93902f0cfebd713df0da67e0a
nikan <nikan.nik83@gmail.com>
2020-03-28 19:54:33 +0800

    d

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 import discord
 import praw
+import random
 
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='>')
+bot = commands.Bot(command_prefix='~')
 
 reddit = praw.Reddit(client_id='Wu-95k2TYxo_cg',
                      client_secret='gZ5X5PKYNsHc0nWD68TYOjxzwck',
                      user_agent='discord')
 
+sub = reddit.subreddit('showerthoughts')
 
 
+@bot.command()
+async def post(ctx):
+    posts = [post for post in sub.hot(limit=100)]
+    random_post_number = random.randint(0, 10)
+    random_post = posts[random_post_number]
+    random_post = random_post.title
+    await ctx.send(random_post)
 
-# hot_posts = reddit.subreddit('showerthoughts').hot(limit=1)
-# for post in hot_posts:
-#     print(post.title)
-random_submission = reddit.subreddit('showerthoughts').random()
-print(random_submission)
-# for submission in reddit.subreddit('showerthoughts').random_rising():
-#     print(submission.title)
-
-
-
-# @bot.command()
-# async def ping(ctx):
-#     await ctx.send('pong')
-# 
-# bot.run('NjkzMzM1NTM4OTU4MjA1MDI4.Xn8DgQ.RDuHV18h60oIlL4GPRKTdQjixBM')
+bot.run('NjkzMzM1NTM4OTU4MjA1MDI4.Xn8DgQ.RDuHV18h60oIlL4GPRKTdQjixBM')
