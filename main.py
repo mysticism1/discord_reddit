4495a0266a02cdcd7055e32a262f680000749f30
nikan <nikan.nik83@gmail.com>
2020-03-29 12:18:18 +0800

    d

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import discord
-import praw
+# import praw
 import random
 
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='~')
 
-reddit = praw.Reddit(client_id='Wu-95k2TYxo_cg',
-                     client_secret='gZ5X5PKYNsHc0nWD68TYOjxzwck',
-                     user_agent='discord')
+# reddit = praw.Reddit(client_id='Wu-95k2TYxo_cg',
+#                      client_secret='gZ5X5PKYNsHc0nWD68TYOjxzwck',
+#                      user_agent='discord')
 
-sub = reddit.subreddit('showerthoughts')
+# sub = reddit.subreddit('showerthoughts')
 
 
 @bot.command()
 async def post(ctx):
-    posts = [post for post in sub.hot(limit=100)]
-    random_post_number = random.randint(0, 10)
-    random_post = posts[random_post_number]
-    random_post = random_post.title
-    await ctx.send(random_post)
+    # posts = [post for post in sub.hot(limit=100)]
+    # random_post_number = random.randint(0, 10)
+    # random_post = posts[random_post_number]
+    # random_post = random_post.title
+    await ctx.send('d')
 
 
 @bot.command()
